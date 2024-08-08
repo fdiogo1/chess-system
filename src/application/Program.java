0f68f7cdfc141f1e1d9e804e40c121524a6bb606
Diogo Ferreira <pessoaldiogoferreira@gmail.com>
2024-08-08 09:11:57 -0300

    BoardExceptions and defensive programming

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import boardgame.BoardException;
 import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		try {
 		ChessMatch match = new ChessMatch();
 		UI.printBoard(match.getPieces());
+		}
+		catch (BoardException e) {
+			System.out.println(e.getMessage());
+		}
 		
 	}
 
